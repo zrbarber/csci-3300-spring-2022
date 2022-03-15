ebe112c6b33b832441cfbac798408cee61b7b4f7
zrbarber <zachbarber10@gmail.com>
2022-03-15 09:05:20 -0500

    Updates Advanced Loops

--- 10_Advanced_Loops/script.js
+++ 10_Advanced_Loops/script.js
@@ -1,13 +1,15 @@
 function letsgo() {
+    const NUMBER_OF_VALUES = 7;
+    let total = 0;
     let message = "<ul>";
 
-    for (let i = 0; i < 5;) {
+    for (let i = 0; i < NUMBER_OF_VALUES;) {
         let number = prompt("Enter a number.");
         number = parseFloat(number);
 
         if (isNaN(number)) {
             alert("That wasn't a number!");
-        } 
+        }
         else {
             if (number % 2 == 0) {
                 message += `<li>${number} is even</li>`;
@@ -15,12 +17,17 @@ function letsgo() {
             if (number % 2 == 1) {
                 message += `<li>${number} is odd</li>`;
             }
-            
+
+            total += number;
             i++;
         }
     }
 
+    let average = total / NUMBER_OF_VALUES;
+
     message += "</ul>";
+    message += `<p>The total of the values is ${total}.</p>`;
+    message += `<p>The average of the values is ${average}.</p>`;
 
     document.getElementById("results").innerHTML = message;
 }
